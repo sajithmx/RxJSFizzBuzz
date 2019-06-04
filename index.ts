d877e2456ba5799c4b25ba1ce4690129516f9d40
sajithmx <sajithm@sajithm.in>
2019-06-04 12:08:52 +0530

    Added: 6th Approach

--- index.ts
+++ index.ts
@@ -1,12 +1,73 @@
-import { of, range, interval } from 'rxjs'; 
-import { map, filter, tap, take, skip } from 'rxjs/operators';
+import { of, range, interval, merge, asyncScheduler } from 'rxjs';
+import { map, filter, tap, take, skip, mapTo } from 'rxjs/operators';
 
 console.clear();
 
-let isFizz = (i) => !(i%3);
-let isBuzz = (i) => !(i%5); 
+let isFizz = (i) => !(i % 3);
+let isBuzz = (i) => !(i % 5);
 
-// Attempt 3: Success
+// Attempt 6: Success
+const source = range(1, 15, asyncScheduler);
+
+const fizz = source.pipe(
+  filter(isFizz),
+  tap(x => console.log(`%cFizz ${x}`, 'font-weight: bold; color: red')),
+  map(x => `%cFizz ${x}`)
+);
+
+const buzz = source.pipe(
+  filter(isBuzz),
+  tap(x => console.log(`%cBuzz ${x}`, 'font-weight: bold; color: green')),
+  map(x => `%cBuzz ${x}`)
+);
+
+merge(fizz, buzz)
+  .subscribe(x => {
+    console.log(`${x}`, 'font-weight: bold; color: blue');
+    console.log('');
+  });
+
+/* // Attempt 5: Partial
+const source = range(1, 15);
+
+const fizz = source.pipe(
+  filter(isFizz),
+  tap(x => console.log(`%cFizz ${x}`, 'font-weight: bold; color: red')),
+  map(x => `%cFizz ${x}`)
+);
+
+const buzz = source.pipe(
+  filter(isBuzz),
+  tap(x => console.log(`%cBuzz ${x}`, 'font-weight: bold; color: green')),
+  map(x => `%cBuzz ${x}`)
+);
+
+merge(fizz, buzz)
+  .subscribe(x => {
+    console.log(`${x}`, 'font-weight: bold; color: blue');
+    console.log('');
+  }); */
+
+/* // Attempt 4: Success
+const source = interval(10);
+
+const fizz = source.pipe(
+  filter(isFizz),
+  // tap(x => console.log(`%cFizz ${x}`, 'font-weight: bold; color: red')),
+  map(x => `%cFizz ${x}`)
+);
+
+const buzz = source.pipe(
+  filter(isBuzz),
+  // tap(x => console.log(`%cBuzz ${x}`, 'font-weight: bold; color: green')),
+  map(x => `%cBuzz ${x}`)
+);
+
+merge(fizz, buzz)
+  .pipe(take(20))
+  .subscribe(x => console.log(`${x}`, 'font-weight: bold; color: blue')); */
+
+/* // Attempt 3: Success
 const source = interval(10);
 
 source.pipe(
@@ -14,7 +75,7 @@ source.pipe(
   take(30),
   tap(x => isFizz(x) && console.log(`%cFizz ${x}`, 'font-weight: bold; color: red')),
   tap(x => isBuzz(x) && console.log(`%cBuzz ${x}`, 'font-weight: bold; color: green'))
-).subscribe();
+).subscribe(); */
 
 /* // Attempt 2: Success
 const source = range(1, 15);
