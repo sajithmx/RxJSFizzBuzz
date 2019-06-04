1f7dcc4931f506a9126e9d134d9521dccde62949
sajithmx <sajithm@sajithm.in>
2019-06-04 11:27:01 +0530

    Attempt 2: Success

--- index.ts
+++ index.ts
@@ -1,11 +1,21 @@
 import { of, range } from 'rxjs'; 
-import { map, filter } from 'rxjs/operators';
+import { map, filter, tap } from 'rxjs/operators';
 
 console.clear();
 
 let isFizz = (i) => !(i%3);
 let isBuzz = (i) => !(i%5); 
 
+// Attempt 2: Success
+const source = range(1, 15);
+
+source.pipe(
+  tap(x => isFizz(x) && console.log(`%cFizz ${x}`, 'font-weight: bold; color: red')),
+  tap(x => isBuzz(x) && console.log(`%cBuzz ${x}`, 'font-weight: bold; color: green'))
+).subscribe();
+
+/* 
+// Attempt 1: Partial
 const source = range(0, 10);
 
 source.pipe(
@@ -16,11 +26,4 @@ source.pipe(
 source.pipe(
   filter(isBuzz),
   map(x => `Buzz ${x}`)
-).subscribe(x => console.log(x));
-
-/* source.pipe(
-  filter(isFizz),
-  map(x => `Fizz`)
 ).subscribe(x => console.log(x)); */
-
-// source.subscribe(x => console.log(x));
